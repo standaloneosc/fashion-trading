7c9df0272fb4be4db7a9828282fe7fb66fc4fd34
standaloneosc <oscardmelendez7@gmail.com>
2026-03-12 21:27:38 -0400

    extend simulator

--- src/backtest/simulator.py
+++ src/backtest/simulator.py
@@ -4,3 +4,8 @@ from dataclasses import asdict
 
 import numpy as np
 import pandas as pd
+
+from src.backtest.metrics import summarize_trades
+from src.config import SETTINGS
+from src.models.sale_prob import predict_sale_probability
+from src.models.survival import predict_daily_hazard
