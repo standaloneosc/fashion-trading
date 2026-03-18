d2a9503079f980404d9e76f07b55e35c48bc06ca
standaloneosc <oscardmelendez7@gmail.com>
2026-03-18 07:30:14 -0400

    adjust stress tests

--- src/backtest/stress.py
+++ src/backtest/stress.py
@@ -4,3 +4,6 @@ import pandas as pd
 
 from src.backtest.simulator import run_strategy_backtest
 from src.config import SETTINGS
+
+
+def run_stress_tests(strategies, listings: pd.DataFrame, sale_model, survival_model) -> pd.DataFrame:
