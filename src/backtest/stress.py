5e9069e81202d57e389be42ff9164aa1fec696c7
standaloneosc <oscardmelendez7@gmail.com>
2026-03-19 00:27:58 -0400

    expand stress tests

--- src/backtest/stress.py
+++ src/backtest/stress.py
@@ -13,3 +13,6 @@ def run_stress_tests(strategies, listings: pd.DataFrame, sale_model, survival_mo
         ("demand_shock", SETTINGS.demand_shock_scale, 1.0, 1.0),
         ("regime_shift", 1.0, SETTINGS.regime_shift_dispersion_scale, 1.0),
         ("adversarial_noise", 1.0, 1.0, SETTINGS.adversarial_price_scale),
+    ]
+    for strategy in strategies:
+        for scenario_name, hazard_scale, dispersion_scale, price_noise_scale in scenarios:
