65677199130fb4d4c10b4db2d77beecb8b306070
standaloneosc <oscardmelendez7@gmail.com>
2026-03-23 11:53:36 -0400

    adjust pipeline

--- run_pipeline.py
+++ run_pipeline.py
@@ -11,3 +11,10 @@ from src.backtest.stress import run_stress_tests
 from src.collect.io import load_market_data
 from src.config import PLOTS_DIR, REPORTS_DIR, SETTINGS, ensure_directories
 from src.features.build_features import build_feature_store, build_training_frame
+from src.models.sale_prob import fit_sale_probability_model
+from src.models.survival import empirical_survival_curve, fit_survival_model
+from src.plots.make_plots import make_plots
+from src.strategies.strategies import STRATEGIES
+
+
+def write_readme_summary(
