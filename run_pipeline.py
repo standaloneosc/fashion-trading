187cb4baa73266f5e3bd45718f9bf910b69f6af5
standaloneosc <oscardmelendez7@gmail.com>
2026-03-23 06:14:22 -0400

    extend pipeline

--- run_pipeline.py
+++ run_pipeline.py
@@ -5,3 +5,9 @@ from pathlib import Path
 
 import pandas as pd
 
+from src.backtest.metrics import aggregate_rollout_summaries
+from src.backtest.simulator import run_strategy_backtest
+from src.backtest.stress import run_stress_tests
+from src.collect.io import load_market_data
+from src.config import PLOTS_DIR, REPORTS_DIR, SETTINGS, ensure_directories
+from src.features.build_features import build_feature_store, build_training_frame
